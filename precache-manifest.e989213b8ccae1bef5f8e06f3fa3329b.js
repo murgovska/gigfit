8a887956925cc836d0297917b5d7233980309bf9
murgovska <kristina.murgovska@gmail.com>
2019-06-21 09:34:31 +0200

    Build files

---
+++ precache-manifest.e989213b8ccae1bef5f8e06f3fa3329b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e19bd87774d927553e4",
+    "url": "/gigfit/static/css/main.2ff2956a.chunk.css"
+  },
+  {
+    "revision": "4e19bd87774d927553e4",
+    "url": "/gigfit/static/js/main.4f1a6d08.chunk.js"
+  },
+  {
+    "revision": "9929e0e4cbd9d0f01109",
+    "url": "/gigfit/static/js/runtime~main.3c58bde3.js"
+  },
+  {
+    "revision": "5c09eca5d86ec3957a41",
+    "url": "/gigfit/static/js/2.115b30bd.chunk.js"
+  },
+  {
+    "revision": "6fc6e29fa28f280a22f5bac030ffb7b1",
+    "url": "/gigfit/static/media/Dots.6fc6e29f.svg"
+  },
+  {
+    "revision": "4f9b02eedb655da15c90e1c6f2841610",
+    "url": "/gigfit/static/media/GiG_FIT.4f9b02ee.svg"
+  },
+  {
+    "revision": "70bf4dd8c7016de3d15ee1c6bf0fa467",
+    "url": "/gigfit/static/media/Close.70bf4dd8.svg"
+  },
+  {
+    "revision": "e417c9ad4bec6d2f7c0676d5ce9a7526",
+    "url": "/gigfit/static/media/Facebook.e417c9ad.svg"
+  },
+  {
+    "revision": "a6fb019216a3a6ce623decff8244282a",
+    "url": "/gigfit/static/media/Instagram.a6fb0192.svg"
+  },
+  {
+    "revision": "89357ffad6d58a5e131a40188f509da9",
+    "url": "/gigfit/static/media/Youtube.89357ffa.svg"
+  },
+  {
+    "revision": "44dce2994d046f6a5e8099680feacd06",
+    "url": "/gigfit/static/media/Snapchat.44dce299.svg"
+  },
+  {
+    "revision": "2beccf5ee62eac46c11887166b8d7e09",
+    "url": "/gigfit/static/media/Burger.2beccf5e.svg"
+  },
+  {
+    "revision": "2e6f5c0985dc5ab898d9af9e5f221217",
+    "url": "/gigfit/static/media/Calculator.2e6f5c09.svg"
+  },
+  {
+    "revision": "3cd3e173b8b2f04e1db8905a2d788239",
+    "url": "/gigfit/static/media/Money.3cd3e173.svg"
+  },
+  {
+    "revision": "636e1a9292e9e355097027e748b6092d",
+    "url": "/gigfit/static/media/Measure.636e1a92.svg"
+  },
+  {
+    "revision": "0986a329134854f07516dca3bcf0680b",
+    "url": "/gigfit/static/media/Money_Bag.0986a329.svg"
+  },
+  {
+    "revision": "be844a8a405eaa26add66c6c2c1c0662",
+    "url": "/gigfit/static/media/18_Plus.be844a8a.svg"
+  },
+  {
+    "revision": "881735912e69c156f2503be99c78ac9d",
+    "url": "/gigfit/static/media/Back.88173591.svg"
+  },
+  {
+    "revision": "c3ded064b87e2f456fb69038e1affd86",
+    "url": "/gigfit/static/media/European_Commision.c3ded064.svg"
+  },
+  {
+    "revision": "419b4cd700f742ce8a1ac2becaee99f5",
+    "url": "/gigfit/static/media/FacebookWhite.419b4cd7.svg"
+  },
+  {
+    "revision": "ef420220a5388291758a40d3d2674d42",
+    "url": "/gigfit/static/media/Gogle.ef420220.svg"
+  },
+  {
+    "revision": "501daa2d6965ca3ede970cb46b70052b",
+    "url": "/gigfit/static/media/McAfee.501daa2d.svg"
+  },
+  {
+    "revision": "ee7058335400dc895f17317b54d094b7",
+    "url": "/gigfit/static/media/man-fat.ee705833.png"
+  },
+  {
+    "revision": "48f89e354438bc2cd27353cc07335352",
+    "url": "/gigfit/static/media/Daily_Mail.48f89e35.svg"
+  },
+  {
+    "revision": "92ed9c991c223a40d2928b270c89d67e",
+    "url": "/gigfit/static/media/man-muscular.92ed9c99.png"
+  },
+  {
+    "revision": "6a5e8a85a2145d2f60a28e633ac0ddff",
+    "url": "/gigfit/static/media/gir-curly.6a5e8a85.png"
+  },
+  {
+    "revision": "f71bdae7e6c6baa4432cafb05af4adc6",
+    "url": "/gigfit/static/media/man-phone.f71bdae7.png"
+  },
+  {
+    "revision": "acb0a7b204b233d7e5797527d5fbeb35",
+    "url": "/gigfit/static/media/Bullet.acb0a7b2.svg"
+  },
+  {
+    "revision": "e53e87cf6672f49e26e177f1c1b7429c",
+    "url": "/gigfit/static/media/Minus.e53e87cf.svg"
+  },
+  {
+    "revision": "5b3c4d1fa26a8f7c7f4ccfb3ed75802e",
+    "url": "/gigfit/static/media/Play.5b3c4d1f.svg"
+  },
+  {
+    "revision": "a1e4cc83a00e656f1af2a861818f2462",
+    "url": "/gigfit/static/media/Plus.a1e4cc83.svg"
+  },
+  {
+    "revision": "eb12882dea7e7ab6c9e05218d23becb4",
+    "url": "/gigfit/static/media/Thumbnail1.eb12882d.png"
+  },
+  {
+    "revision": "3c9bc5cdaa983a4c0c1f745b84602e07",
+    "url": "/gigfit/static/media/Thumbnail2.3c9bc5cd.png"
+  },
+  {
+    "revision": "4f32514cfd6dcb63d0ee7c9f7c9f8eeb",
+    "url": "/gigfit/static/media/Thumbnail3.4f32514c.png"
+  },
+  {
+    "revision": "67b5b5b2e5effe9a0168e7faa7b8a693",
+    "url": "/gigfit/static/media/Thumbnail4.67b5b5b2.png"
+  },
+  {
+    "revision": "5800d5ee865b2e2dcf7498cc8aefd4f4",
+    "url": "/gigfit/static/media/Thumbnail5.5800d5ee.png"
+  },
+  {
+    "revision": "c576fd5e2e77f770c50299656ae18c1f",
+    "url": "/gigfit/index.html"
+  }
+];
